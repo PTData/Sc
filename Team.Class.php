d42055072c5f9bf5b4ac52b1948e348bc95f3c12
Pedro <ptdata@gmail.com>
2013-08-16 11:01:58 +0100

    Update Team.Class.php
    actualizações

--- Team.Class.php
+++ Team.Class.php
@@ -1,9 +1,6 @@
 <?php
 include 'Player.Class.php';
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+include 'Especifico.php';
 
 /**
  * Description of Team
@@ -11,21 +8,23 @@ include 'Player.Class.php';
  * @author pedrodata
  */
 class Team {
+    
     private $player = array();
     private $idade, $valor, $qualidade;
+    private $db;
+    
     function __construct() {
-       
-       $this->qualidade = rand();
-       for ($i = 0; $i < 11; $i ++) {
-           $this->idade = rand(16, 35);
-           $this->qualidade = rand(0, 50);
-           $this->player[$i][] = new Player($this->nomes(), $this->idade, $valor, $this->qualidade);
-       }
-       
+       $this->db = new Ispecifico();
+    }
+     
+    private function positions() {
+        $gk = 'guarda-redes';
+        $def = 'defesa';
+        $med = 'médio';
+        $for = 'avançado';
     }
     
-    
-    function nomes() {
+    private function nomes() {
        $nome = array('Rodrigo', 'Joaquim', 'Inácio', 'Feliciano', 'Pedro', 'Rodrigo', 'Augusto', 'José',
            'Cristovão', 'Bernardo', 'Sílvio', 'Tiago', 'Nelson', 'Hugo', 'Miguel', 'André', 'Luis', 'Carlos', 'Alberto');
        $array_nome = array_rand($nome, 1);
@@ -35,10 +34,45 @@ class Team {
        return $nome[$array_nome] . ' ' . $apelido[$array_apelido];
     }
     
-    function players() {
-        echo '<pre>';
-        print_r($this->player);
-        echo '</pre>';
+    public function players($team) {
+        if(!(int) $team) die('tem de ser valor numérico');
+        $query = "Select id_player, name_player, age_player from players where idteam_player = ?";
+        //$query = "Select name_player, age_player, quality_player from players where name_player LIKE= ?";
+        $equipa = $this->db->select_prepare($query, $team);
+        
+        $this->db->close();
+        return $equipa;
+    }
+    
+    public function insert_players($team) {
+       
+       $valor = 5;
+       $sql = "INSERT INTO players (name_player, age_player, position_player, value_player, quality_player, idteam_player)";
+       $sql .= " VALUES";
+       for ($i = 0; $i < 11; $i ++) {
+           $this->idade = rand(16, 35);
+           $this->qualidade = rand(0, 50);
+           $sql .= " ('".$this->nomes()."', '".$this->idade."', '".$this->positions()."','".$valor."', '".$this->qualidade."', $team )";
+
+           if($i < 10) $sql .= ",";
+       }
+       $this->db->insert($sql);
+    }
+    
+    public function select_all_players($page) {
+        $start = ($page - 1) * 15;
+        $players = $this->db->select("select id_player, name_player, age_player, value_player from players limit $start, 15");
+        
+        $json = array();
+            $count = 0;
+            foreach($players as $j) {
+               //$json['players']['count']= $count;
+               $json['players'][]['info'] = $j; 
+               //$count ++;
+            }
+        
+        return json_encode($json, true);
+        //echo $json;
     }
 }
 
