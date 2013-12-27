2ee25810ce10ca67c9cd8c592961a84359872a56
Pedro <ptdata@gmail.com>
2013-12-27 00:17:27 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,43 +1,37 @@
-<?php
-include './Especifico.php';
-//$hora = new Horas('2013-04-22 12:29:17');
-//$hora->get_horas();
-
-//include './db.class.php';
-//db::nova_ligacao('localhost', 'root', 'eurorscg', 'data_db');
-
-//$db= new Conexao();
-//$db->select('tabela', array('texto'));
-
-$db = new Ispecifico();
-
-$resultado = $db->select('select * from pessoa');
-$db->printr($resultado);
-
-$rs = $db->select('select * from pessoa where id_pessoa = 5');
-$db->printr($rs);
-# update
-//$update = $db->update("insert into pessoa (nome_pessoa, sexo_pessoa, data_pessoa) values('Pedro', '2', '2013-04-29 00:00:00')", TRUE);
-
-
-
-$db->close();
-
-## League
+<?php 
+error_reporting(E_ALL);
 include './Classes/Team.Class.php';
-$team = new Team();
-$team->players();
-
-
-## para jogar
-/*
-include './Classes/Jogo.Class.php';
-
-$equipa1 = array('Sacotes', 55);
-$equipa2 = array('Psiqur', 66);
-
-
-$jogo = new Jogo($equipa1, $equipa2);
-$jogo->jogar()
-*/
+$id = isset($_REQUEST['id']) ? $_REQUEST['id'] : 0;
+$page = isset($_REQUEST['page']) ? $_REQUEST['page'] : '';
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <style type="text/css">
+            body {background-color:#996600;}
+            p {color:blue;}
+            ul {width: 730px}
+            ul li {float: left; list-style: none; width: 170px}
+            .clear {clear: both}
+        </style>
+        <title></title>
+    </head>
+    <body>
+      
+        <?php 
+        if(isset($page) && $page !='') {
+            
+            switch ($page) {
+                case 'team' :
+                    include('team.php');
+                    break;
+                case 'jogo':
+                    include('jogo');
+                    break;
+            }
+        }
+        ?>
+       
+    </body>
+</html>
